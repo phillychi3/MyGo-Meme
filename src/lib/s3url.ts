b64d3f880a7f725469b9d31fb19dd6f6906b5e1d
phillychi3 <phillychi3@gmail.com>
2025-03-03 02:04:19 +0800

    fux url

--- src/lib/s3url.ts
+++ src/lib/s3url.ts
@@ -1,2 +1,2 @@
-export const s3url = "https://s3.whitecloud.life/animelines/";
+export const s3url = "https://r2.cloudowo.com/animelines/";
 export const s3devurl = "https://pub-b788b26a3d8b42888cac7fac3e3a84bb.r2.dev/animelines/"
